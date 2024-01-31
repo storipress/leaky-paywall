a45bd49382423f0554043d969867181df31947df
David Peng <dododavid006@gmail.com>
2024-01-31 16:26:06 +0800

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    stylistic: false,
+    rules: {
+      'antfu/top-level-function': 'error',
+
+      'vue/html-indent': 'off',
+      'vue/html-closing-bracket-newline': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+    },
+  },
+  {
+    ignores: ['src/vite-env.d.ts'],
+  },
+)
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': '/src/',
+    },
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', '@vueuse/core'],
+      dirs: ['src/utils'],
+    }),
+    Components({
+      dirs: ['src/components', 'src/components/ui'],
+    }),
+    VueDevTools(),
+  ],
+})
