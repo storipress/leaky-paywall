86c92fd0cd988ed9dc7a6186721801141ca062f5
DanSnow <dododavid006@gmail.com>
2024-02-29 17:18:01 +0800

    build: setup terraform

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.38.0"
+    }
+  }
+
+  cloud {
+    organization = "Storipress"
+    workspaces {
+      name = "leaky-paywall"
+    }
+  }
+
+  required_version = "1.5.7"
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_s3_object" "leaky_paywall" {
+  bucket      = "storipress"
+  key         = "assets/storipress/leaky-paywall.min.js"
+  source      = "../lib/leaky-paywall.min.js"
+  source_hash = filemd5("../lib/leaky-paywall.min.js")
+}
