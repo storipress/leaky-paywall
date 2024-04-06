6b269f27ca99bb3b40c84222c0c0395fa881a257
DanSnow <dododavid006@gmail.com>
2024-04-06 16:06:19 +0800

    docs: add comment

--- packages/script-provider/src/index.ts
+++ packages/script-provider/src/index.ts
@@ -3,6 +3,7 @@ import { oneLineTrim } from 'proper-tags'
 import { Effect, SynchronizedRef, pipe } from 'effect'
 import esbuild from 'esbuild-wasm'
 import { SiteSubscriptionInfo } from 'storipress-client'
+// ref: https://blog.interactions.rest/blog/esbuild-on-the-edge/
 // @ts-expect-error no type
 // eslint-disable-next-line antfu/no-import-node-modules-by-path
 import wasm from '../../../node_modules/esbuild-wasm/esbuild.wasm'
