90f3003544f65452ba6b773ca829ff8e9e727f9e
DanSnow <dododavid006@gmail.com>
2024-05-17 13:34:39 +0800

    test: add test

--- packages/script-provider/src/utils/__tests__/get-paywall.config.spec.ts
+++ packages/script-provider/src/utils/__tests__/get-paywall.config.spec.ts
@@ -26,6 +26,11 @@ it('can response config json', async () => {
                     interval: 7,
                     quota: 7,
                   },
+                  paywall_trigger: {
+                    type: 'viewport',
+                    offset: 0.45,
+                  },
+                  hit_limit_title: '',
                   hit_limit_cta: 'Hit limit cta',
                   logo: 'https://example.com/logo.png',
                 } satisfies PaywallConfig),
@@ -51,6 +56,66 @@ it('can response config json', async () => {
     freeLimit: {
       interval: 7,
       quota: 7,
+    },
+    paywallTrigger: {
+      type: 'viewport',
+      value: 0.45,
+    },
+    description: 'Hit limit cta',
+    logo: 'https://example.com/logo.png',
+  } satisfies Config)
+})
+
+it('can response config json without new config', async () => {
+  const res = await pipe(
+    getPaywallConfig('client_id'),
+    Effect.provideService(
+      GraphqlService,
+      GraphqlService.of({
+        query: () =>
+          Effect.succeed({
+            hasNext: false,
+            operation: {} as any,
+            stale: {} as any,
+            data: {
+              siteSubscriptionInfo: {
+                name: 'Site Name',
+                paywall_config: JSON.stringify({
+                  brand_color: '#ff0000',
+                  dismissible: true,
+                  free_limit: {
+                    interval: 7,
+                    quota: 7,
+                  },
+                  hit_limit_cta: 'Hit limit cta',
+                  logo: 'https://example.com/logo.png',
+                } as unknown as PaywallConfig),
+              },
+            } as any,
+          }),
+      }),
+    ),
+    Effect.runPromise,
+  )
+
+  expect(res).toEqual({
+    flags: {
+      paywall: true,
+      tracking: true,
+    },
+    all: false,
+    clientId: 'client_id',
+    title: 'Site Name',
+    primaryColor: '#ff0000',
+    dismissible: true,
+    pathPattern: null,
+    freeLimit: {
+      interval: 7,
+      quota: 7,
+    },
+    paywallTrigger: {
+      type: 'viewport',
+      value: 0.45,
     },
     description: 'Hit limit cta',
     logo: 'https://example.com/logo.png',
