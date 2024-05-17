4f09e1a6e722ef25b4f0802f95199e75e3d1fded
DanSnow <dododavid006@gmail.com>
2024-05-17 13:37:55 +0800

    chore: remove unused script

--- scripts/deploy.ts
+++
@@ -1,9 +0,0 @@
-import { $ } from 'zx'
-
-$.verbose = true
-
-await $({ stdio: 'inherit' })`moon run shared:build`
-await $`vite build`
-await $`vite build --mode=lib`
-await $`node esbuild.config.js`
-await $`cp lib/leaky-paywall.min.js dist`
